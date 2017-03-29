943085f11eb940f7f24c54ad370e090954e67bbe
Danny Glover <danny@plastiq.com>
2017-03-28 21:38:27 -0700

    Initial Commit

---
+++ Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by Danny Glover on 3/28/17.
+//  Copyright © 2017 Danny Glover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
